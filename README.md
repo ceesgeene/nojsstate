765c99e94a49fb442898a7a4e534ad9c17190189
ceesgeene <ceesgeene@gmail.com>
2015-06-03 20:57:33 +0200

    initial code and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# nojsstate
+# pagestate
+
+Using the CSS target: pseudo selector it is possible to apply style variations based upon the fragment identifier of the
+URI of a HTML document.
+
+A benefit of using the fragment identifier of the URI to set a web page into a specific state (like showing a sidebar)
+is that it adds a record to the browser's history; users can use the back-button of the browser to return to the
+previous state.
+
+A problem is however that it adds a record every time the fragment identifier changes. Opening and
+closing the sidebar multiple times will add an equal amount of records to the browser history. Using the browser's
+back-button will also open and close the sidebar as many times.
+
+Pagestate.js ensures that only the last browser history record is a "page state".
---
+++ pagestate.js
@@ -0,0 +1,59 @@
+(function (window, location, history) {
+    window.addEventListener('hashchange', onHashchange);
+    window.addEventListener('click', onClick);
+
+    var pageHistory = false;
+
+    function onHashchange() {
+        pageHistory = true;
+
+        // Prevent scrolling to top of page.
+        if (location.hash === '') {
+            return false;
+        }
+    }
+
+    function onClick(event) {
+        // No need to continue if the window's location isn't going to be updated.
+        if (event.defaultPrevented) {
+            return;
+        }
+
+        var target = event.target;
+        // Find closest link element.
+        while (target.tagName !== 'A' && target !== document.body) {
+            target = parent.parentNode;
+        }
+        // Stop early if target is not a link or page has no state.
+        if (target.tagName !== 'A' || location.hash.substr(0, 7) !== '#state-') {
+            return;
+        }
+        var linkHashPos = target.href.indexOf('#');
+        // Stop if link has no hash.
+        if (linkHashPos === -1) {
+            return;
+        }
+        var windowUrl = location.href,
+            windowHashPos = windowUrl.indexOf('#'),
+            windowUrlWithoutHash = windowUrl.substr(0, windowHashPos !== -1 ? windowHashPos : undefined);
+        // Only apply behavior if the link matches the current url (ignoring hashes) and we have page state.
+        if (target.href.substr(0, linkHashPos) === windowUrlWithoutHash) {
+            event.preventDefault();
+            if (target.hash) {
+                // Use location.replace() instead of history.replaceState() because the latter does not rematch "target:"
+                // pseudo selectors (at least not in webkit https://bugs.webkit.org/show_bug.cgi?id=83490).
+                location.replace(target.hash);
+            }
+            // Empty hash while in page state means we want to remove the state from the history all together.
+            else if (pageHistory) {
+                history.go(-1);
+            }
+            // We don't know whether we can go back in history, so just remove the hash from the url.
+            else {
+                location.replace('#');
+                history.replaceState({}, '', windowUrlWithoutHash)
+            }
+        }
+    }
+
+})(window, location, history);
